7cbb54237199d92b5acf38793557a63471ef7c17
“[Techy <connectnowwithrahul@gmail.com>
2022-09-11 23:19:41 +0530

    Nothing but just some theoretical topics added for deep knowledge of C language

---
+++ 1_Introduction/1_Why C/why.c
@@ -0,0 +1,5 @@
+// Hey there,
+
+// Are you interested in knowing why to start with C, why to choose C, why you are leaerning C then you can go through by blog in the series of C and Cpp. There you will find the blogs for the upcoming topics with explanation.
+
+// See you there...
---
+++ 1_Introduction/2_Histroy of C/history.c
@@ -0,0 +1,5 @@
+// Hey there,
+
+// Are you interested in knowing history of C language then you can go through by blog in the series of C and Cpp. There you will find the blogs for the upcoming topics with explanation.
+
+// See you there...
