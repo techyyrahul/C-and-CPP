66669dd2f53341841e39d5ffdfc36241b9807f3c
“[Techy <connectnowwithrahul@gmail.com>
2022-09-11 16:47:41 +0530

    First program of C language

---
+++ 1_Introduction/3_Basic Programs/first_program.c
@@ -0,0 +1,28 @@
+// This is the first program of C language in which we will learn how to print something
+
+// Online Compiler
+#include<stdio.h>
+
+void main()
+{
+    printf("Hello World");
+}
+
+
+
+// Turbo C++
+
+// #include<stdio.h>
+// #include<conio.h>
+
+// void main()
+// {
+//     clrscr();
+//     printf("Hello World");
+//     getch();
+// }
+
+
+// You can copy the code and remove the two '//' for turbo c++
+
+// Here the two forward slash // stands for comments which are not read by machine but just by us who read and write the code
